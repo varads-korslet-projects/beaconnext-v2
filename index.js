3cab7181633a8c5a04550cc3d73a2fcb8bf1322f
Varad <varad.gcs@gmail.com>
2024-01-19 04:26:58 +0530

    index: Move to express-rate-limit
    Replace 'body-parser' with 'express-rate-limit' and configure rate limiting. Adjust middleware settings for improved security and IP address handling.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express= require('express')
-const bodyParser= require('body-parser')
 const cors= require('cors')
+const rateLimit = require('express-rate-limit')
 const {readdirSync} = require('fs')
 require('dotenv').config()
 const mongoose = require('mongoose')
@@ -9,6 +9,11 @@ const mongoose = require('mongoose')
 //app
 const app = express()
 
+// Rate Limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 Mins
+    max: 500
+})
 
 //db
 mongoose.connect(process.env.DATABASE,{
@@ -19,9 +24,9 @@ mongoose.connect(process.env.DATABASE,{
 .catch(err => console.log("DB CONNECTION ERROR", err));
 
 //middleware
-app.use(bodyParser.json({limit:"2mb"}));
+app.use(express.json({limit:"2mb"}));
 app.use(cors());
-app.use(bodyParser.urlencoded({extended:true}));
+app.set('trust proxy', 1);
 
 //routes
 readdirSync('./routes').map((r)=>

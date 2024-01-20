6f3b582033453e65bcc4f6cec1d27db1e5ef1882
Varad <varad.gcs@gmail.com>
2024-01-20 23:56:35 +0530

    authCheck -> authCheckStudent Rename and refactor with regards to addition of authCheckTeacher

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,33 +1,20 @@
-const Student = require('../models/student');
 const jwt = require('jsonwebtoken');
 
-exports.authCheck = async (req, res, next) =>{
-    // console.log(req.headers);
-    // // next();
-    // try{
-    //     const firebaseUser = await admin
-    //     .auth()
-    //     .verifyIdToken(req.headers.authtoken);
-    //     console.log("FIREBASE USER IN AUTHCHECK", firebaseUser);
-    //     req.student = firebaseUser;
-    //     next();
-    // }catch(err){
-    //     res.status(401).json({
-    //         err: "Invalid or expired token ",
-    //     });
-    // }
-
-    const authHeader = req.headers['authorization'];
-    if (!authHeader) {
+exports.authCheckStudent = async (req, res, next) =>{
+  const token = req.headers['authorization'];
+  if (!token) {
         return res.status(401).json({ error: 'Unauthorized - Missing Authorization header' });
       }
       jwt.verify(token, process.env.signingkey, (err, student) => {
         if (err) {
           return res.status(403).json({ error: 'Forbidden - Invalid token' });
        }
-       //teacher role
-       req.student = student;
-       next();
+       if(student.role == "student"){
+        req.student = student;
+        next();
+       }else{
+        return res.status(403).json({ error: 'Forbidden - Unauthorized' });
+       }
     });
 };
 

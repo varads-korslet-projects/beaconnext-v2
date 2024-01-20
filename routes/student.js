dc85535a3198c6d7282c9231b547cbef6790cf53
Varad <varad.gcs@gmail.com>
2024-01-21 00:02:02 +0530

    routes: Student: Refactor and inline with auth

--- routes/student.js
+++ routes/student.js
@@ -3,14 +3,14 @@ const express = require('express')
 const router = express.Router()
 
 //middlewares
-const {authCheck} = require("../middlewares/auth");
+const {authCheckStudent} = require("../middlewares/auth");
 
 //controller
 const {currentStudent,createStudentAccounts, getAll, firstLogin, studentLogin} = require('../controllers/student');
 
-router.post("/current-student", authCheck, currentStudent );
+router.post("/current-student", authCheckStudent, currentStudent );
 router.post("/create-student-accounts",createStudentAccounts);
-router.post("/first-login", firstLogin);
+router.post("/first-student-login", firstLogin);
 router.post("/student-login", studentLogin)
 router.get("/getAll",  getAll);
 module.exports = router; 

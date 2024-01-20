af548b9626de25bc3a14c86cdd316beb178d30bb
Varad <varad.gcs@gmail.com>
2024-01-20 23:58:23 +0530

    models: Add lecture model

---
+++ models/lecture.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose'),
+bcrypt = require('bcrypt')
+const { ObjectId } = mongoose.Schema;
+
+const lectureSchema = new mongoose.Schema({
+    subjectName: {
+       type: String,
+       required: true
+    },
+    teacher:{
+        type: ObjectId,
+        ref: "Teacher",
+    },
+    StartTime:{
+        type: Date,
+        required: true,
+    },
+    EndTime:{
+        type: Date,
+        required: true,
+    },
+    year:{
+        type: Number,
+        required: true,
+    },
+    division:{
+        type: String,
+         max: 2
+    },
+    class:{
+        type: Number,
+        required: true
+    },
+    minimumTime:{
+        type: Number,
+        required: true
+    }
+},
+{timestamps:true}
+);
+
+
+module.exports = mongoose.model('Lecture', lectureSchema)

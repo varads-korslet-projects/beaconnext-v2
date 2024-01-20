53902a2a123658c02c9a5d2578fc55a690d6afff
Varad <varad.gcs@gmail.com>
2024-01-21 00:05:55 +0530

    controllers: Student: inline with auth and add Attendence methods

--- controllers/student.js
+++ controllers/student.js
@@ -2,11 +2,12 @@ var mongoose = require('mongoose'),
   jwt = require('jsonwebtoken'),
   bcrypt = require('bcrypt')
 const Student = require('../models/student')
+const Attendance = require('../models/attendance');
 
 exports.createStudentAccounts = async(req,res) => {
     try {
         const passkey = req.headers['passkey'];
-        if(passkey == process.env.passkey){
+        if(passkey == process.env.passkeyStudent){
             const students = req.body;
             const result = await Student.insertMany(students);
             res.status(201).json(result);
@@ -26,7 +27,7 @@ exports.firstLogin = async (req,res)=>{
         if(student.hash_password == oldPassword ){
             hashed_password = bcrypt.hashSync(newPassword, 10);
             await Student.findOneAndUpdate({moodleId:moodleId}, {hash_password:hashed_password, deviceId: deviceId})
-            const token = jwt.sign({ moodleId: student.moodleId, name: student.name, _id: student._id }, process.env.signingkey)
+            const token = jwt.sign({ moodleId: student.moodleId, name: student.name, _id: student._id , role: "student"},  process.env.signingkey)
             res.status(201).json({success: "Your password was changed successfully", token});
         }
         else{
@@ -48,7 +49,7 @@ exports.studentLogin = async (req,res)=>{
         if(student.deviceId == deviceId){
             const match = await bcrypt.compare(password, student.hash_password)
             if(match){
-                const token = jwt.sign({ moodleId: student.moodleId, name: student.name, _id: student._id }, process.env.signingkey)
+                const token = jwt.sign({ moodleId: student.moodleId, name: student.name, _id: student._id, role: "student" },  process.env.signingkey)
                 res.status(201).json({success: "Login successful", token});
             }
             else{
@@ -57,29 +58,60 @@ exports.studentLogin = async (req,res)=>{
         }else{
             res.status(401).json({error: "Login with the same device"});
         }
-        return res.json({ token: jwt.sign({ email: user.email, fullName: user.fullName, _id: user._id }, process.env.signingkey) });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 }
-// exports.createOrUpdateStudent = async (req,res)=>{
-//     const {email, name, moodleId} =req.student;
-//     const student = await Student.findOneAndUpdate({email}, {name} , {moodleId});
 
-//     if(student){
-//         console.log('STUDENT UPDATED');
-//         res.json(student);
-//     }else{
-//         const newStudent = await new Student({
-//             email,
-//             name,
-//             moodleId
-//         }).save();
-//         console.log("New Student created----");
-//         res.json(newStudent);
-//     }
-// }
+exports.countAttendance = async (req,res)=>{
+    const {lecture} = req.body;
+    const {student} = req.student.Id;
+    try{
+        const lectureDetails = await Lecture.findOne({lecture:lecture,});
+        const currentDate = new Date();
+        if(currentDate >= lectureDetails.StartTime && currentDate <= EndTime){
+            const attendance = await Attendance.findOne({lecture:lecture, "student.Id": student});
+
+        if(!attendance && attendance.students.length == 0 ){
+            const newStudentEntry = {
+                Id: 'student',
+                Count: 0 
+            };
+            const updatedAttendance = await Attendance.findByIdAndUpdate(
+                {lecture: lecture},
+                { $push: { students: newStudentEntry } },
+                { new: true }
+            );
+            res.status(200).json('Created and Counted');
+        }else{
+            const attendee = await Attendance.findOneAndUpdate(
+                 {_id: attendance.Id, "student.Id": student},
+                { $inc: { 'student.Count': 1 } },
+                { new: true } 
+            )
+            if(attendee.student.Count > lectureDetails.minimumTime){
+                await Attendance.findOneAndUpdate(
+                    {_id: attendance.Id, "student.Id": student},
+                    { $set: { "student.Present": true } },
+                   { new: true } 
+               )
+                res.status(200).json('You have been marked present');
+            }else{
+                res.status(200).json('Counted');
+            }
+        }
+        }
+        else{
+            res.status(401).json({ error: 'Lecture is Inactive' });
+        }
+
+    }catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+
+}
 
 exports.currentStudent = async (req, res) => {
     Student.findOne({moodleId: req.student.moodleId}).exec((err, student)=>{

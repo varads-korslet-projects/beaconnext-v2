ca2e5719659f218b907e6b15b3fa9fb3c53452c8
Varad <varad.gcs@gmail.com>
2024-01-30 11:05:31 +0530

    routes/lecture: Add route for ongoing lectures

--- routes/lecture.js
+++ routes/lecture.js
@@ -5,10 +5,11 @@ const router = express.Router()
 //middlewares
 const {authCheckTeacher} = require("../middlewares/auth");
 
-const {createLecture, lectureStatus, upcomingLectures} = require('../controllers/lecture');
+const {createLecture, lectureStatus, upcomingLectures, ongoingLectures} = require('../controllers/lecture');
 
 
 router.post("/create-lecture",authCheckTeacher ,createLecture);
 router.get("/lecture-status", lectureStatus);
 router.get("/upcoming",authCheckTeacher, upcomingLectures);
+router.get("/ongoing",authCheckTeacher, ongoingLectures);
 module.exports = router;

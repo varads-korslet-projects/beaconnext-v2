06de3d7ea9da6dc0cd7beb6b85598ec2f22199a3
Varad <varad.gcs@gmail.com>
2024-02-07 11:13:44 +0530

    controllers/notification: Let teachers specify room number to create notifications automatically. All more comprehensive checks for null values

--- controllers/notification.js
+++ controllers/notification.js
@@ -1,13 +1,29 @@
 const Notification = require("../models/notification")
 const Teacher = require("../models/teacher")
+const Lecture = require("../models/lecture")
 
 exports.createNotification = async(req, res) => {
     const notif = req.body;
-    const currentTeacher = req.teacher._id;
-    if (!Notification || !Teacher) {
+    const currentTeacher = req.teacher;
+    if (!notif || !currentTeacher) {
         return res.status(400).json({status:"Bad Request"});
     }
-    notif.teacher = currentTeacher;
+    if (!notif.title || !notif.description || !notif.EndTime) {
+        return res.status(400).json({status:"Bad Request"});
+    }
+    if (!notif.department && res.body.roomNo){
+        //Find which lecture is going on in given room and populate fields
+        const lecture = await Lecture.findOne({
+            teacher: currentTeacher._id,
+            roomNo:res.body.roomNo,
+            StartTime: { $lt: currentDate },
+            EndTime: { $gt: currentDate }
+        });
+        notif.department = lecture.department;
+        notif.year = lecture.year;
+        notif.division = lecture.division;
+    }
+    notif.teacher = currentTeacher._id;
     const result = await Notification.create(notif);
     return res.status(200).json(result)
 }

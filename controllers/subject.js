eb81fdead7ee55f3c7d1fa87d9a5a278d3b37c7a
Varad <varad.gcs@gmail.com>
2024-02-17 11:19:08 +0530

    subject: Refactor addSubject

--- controllers/subject.js
+++ controllers/subject.js
@@ -1,18 +1,18 @@
 
 const Subject = require('../models/subject')
 
-exports.addSubject = async(req,res) => {
-    try {
-        const passkey = req.headers['passkey'];
-        if(passkey == process.env.passkeyAdmin & !req.body){
-            const subjects = req.body;
-            const result = await Subject.insertMany(subjects);
-            res.status(201).json(result);
-        }else{
-            res.status(500).json({ error: "Wrong passkey" });
-        }
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: error });
+exports.addSubject = async (req, res) => {
+  try {
+      const passkey = req.headers['passkey'];
+      if (passkey == process.env.passkeyAdmin && req.body) {
+          const subjects = req.body;
+          const result = await Subject.insertMany(subjects);
+          res.status(201).json(result);
+      } else {
+          res.status(500).json({ error: "Wrong passkey or empty request body" });
       }
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: error.message });
+  }
 }

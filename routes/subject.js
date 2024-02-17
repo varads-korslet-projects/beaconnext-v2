b8956f27e04dd4b695cbc4b73c028fb6da8e2452
Varad <varad.gcs@gmail.com>
2024-02-17 11:26:00 +0530

    subjects: Add endpoint getSubject method

--- routes/subject.js
+++ routes/subject.js
@@ -3,8 +3,8 @@ const express = require('express')
 const router = express.Router()
 
 //controller
-const {addSubject} = require('../controllers/subject');
+const {addSubject, getSubject} = require('../controllers/subject');
 
 router.post("/add-subjects",addSubject);
-
+router.post("/get-subjects", getSubject)
 module.exports = router; 

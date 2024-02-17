d9c2174fc9e3d37c18029eaef801184788806c30
Varad <varad.gcs@gmail.com>
2024-02-17 11:25:13 +0530

    subjects: Add a method for retriving subjects

--- controllers/subject.js
+++ controllers/subject.js
@@ -7,12 +7,28 @@ exports.addSubject = async (req, res) => {
       if (passkey == process.env.passkeyAdmin && req.body) {
           const subjects = req.body;
           const result = await Subject.insertMany(subjects);
-          res.status(201).json(result);
+          return res.status(201).json(result);
       } else {
-          res.status(500).json({ error: "Wrong passkey or empty request body" });
+          return res.status(500).json({ error: "Wrong passkey or empty request body" });
       }
   } catch (error) {
       console.error(error);
-      res.status(500).json({ error: error.message });
+      return res.status(500).json({ error: error.message });
+  }
+}
+
+exports.getSubject = async (req, res) => {
+  try {
+    if(!req.body.department || !req.body.year || !req.body.division){
+        return res.status(400).json({ status: 'Bad Request'});
+    }
+    const subjects = await Subject.find({
+      department: req.body.department,
+      year: req.body.year,
+      division: req.body.division
+    })
+    return res.status(200).json({subjects})
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
   }
 }

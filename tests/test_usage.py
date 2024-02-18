e256ead0e125789238fe7b28f5e0668ac0a8e8c4
Varad <varad.gcs@gmail.com>
2024-02-18 12:43:11 +0530

    tests: Temporarily drop unused tests

--- tests/test_usage.py
+++
@@ -1,6 +0,0 @@
-import tests
-
-tests.test_get_all()
-tests.test_teacher_login()
-tests.test_create_lecture()
-tests.test_count_attendance()
--- tests/tests_creation.py
+++
@@ -1,7 +0,0 @@
-import tests
-
-tests.test_create_student_accounts()
-tests.test_student_first_login()
-tests.test_student_login()
-tests.test_create_teacher_accounts()
-tests.test_teacher_first_login()
